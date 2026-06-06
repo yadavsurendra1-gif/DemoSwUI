f4b59f5e78f0c38618f90403aa2a1ee9adddc4e4
Surendra <55agency.live@gmail.com>
2026-06-06 13:31:38 +0530

    Initial Commit

---
+++ DemoSwUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DemoSwUI
+//
+//  Created by B2Cvertical A++ on 06/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DemoSwUI/DemoSwUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSwUIApp.swift
+//  DemoSwUI
+//
+//  Created by B2Cvertical A++ on 06/06/26.
+//
+
+import SwiftUI
+
+@main
+struct DemoSwUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
